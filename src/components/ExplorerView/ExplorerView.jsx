269f4bfcd211c47c3889dc543e0660f824f4b707
sandrine MZE <liaworkingonline@gmail.com>
2026-01-13 14:15:00 +0100

    [CLEAN] Explorer view

--- src/components/ExplorerView/ExplorerView.jsx
+++ src/components/ExplorerView/ExplorerView.jsx
@@ -1,9 +1,8 @@
 import React from "react";  
 import IconGrid from "../../containers/iconGrid";
-import ProjectView from "../ProjectView/ProjectView";  
 
 const ExplorerView = ({ node, view }) => {
-  console.log('EXPLORER_VIEW', view, node);
+  
   if (!node) return null;
 
   return <IconGrid items={node.children} />;
