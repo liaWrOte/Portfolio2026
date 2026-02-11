136e91fb10d4bee5b45fd44ee8d10c1b96a8914f
sandrine MZE <liaworkingonline@gmail.com>
2026-02-11 09:19:05 +0100

    [ADD] .env

--- src/middlewares/env.ts
+++ src/middlewares/env.ts
@@ -1,2 +1,5 @@
-export const apiUrl = 'http://localhost:1337/api';
-export const backendUrl = 'http://localhost:1337';
+// Configuration des URLs
+const strapiUrl = 'http://localhost:1337';
+
+export const apiUrl = `${strapiUrl}/api`;
+export const backendUrl = strapiUrl;
